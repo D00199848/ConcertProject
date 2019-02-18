9df6edfe4bc192c0f597f3451f6fe756d6c56707
D00199848 <47359574+D00199848@users.noreply.github.com>
2019-02-18 18:55:33 +0000

    I added a nav bar. I still need to make the tabs horizontal.

--- view/header.php
+++ view/header.php
@@ -2,6 +2,17 @@
 <html>
 <!-- the head section -->
 <head>
+    
+    <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#myPage">HOME</a></li>
+        <li><a href="#band">BANDS</a></li>
+        <li><a href="#tour">GIGS & TICKETS</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+        </ul>
+    </nav>
+        
     <title>Tickets Direct</title>
     <link href="../main.css" rel="stylesheet" type="text/css"/>
 </head>
